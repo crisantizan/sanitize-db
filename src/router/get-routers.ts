a58cd94505f79aa927718eea67e3fdc88684287f
Cristian Santiz <crisantizan@gmail.com>
2020-05-15 08:57:28 -0500

    chore: add comments

--- src/router/get-routers.ts
+++ src/router/get-routers.ts
@@ -2,25 +2,41 @@ import { readdirSync } from 'fs';
 import { join } from 'path';
 import { ControllerRouteProps } from '@/typings/shared.typing';
 
+/** auto import controllers */
 function autoImport() {
+  // path of modules folder
   const modulesPath = join(__dirname, '..', 'modules');
-  const modules = readdirSync(modulesPath);
-  const controllers: any[] = [];
 
-  modules.forEach(folder => {
-    const files = readdirSync(join(modulesPath, folder));
-    const controllerFile = files.filter(f => /\.controller\.ts$/.test(f))[0];
-    const controller = require(`@/modules/${folder}/${controllerFile}`);
+  try {
+    const modulesFolder = readdirSync(modulesPath);
 
-    if (!controller.default) {
-      throw new Error(
-        'Controllers should be exported by: export default, no custom names',
-      );
-    }
-    controllers.push(controller.default);
-  });
+    const controllers: any[] = [];
 
-  return controllers;
+    modulesFolder.forEach(folder => {
+      // browse folder, files inside of the current module
+      const files = readdirSync(join(modulesPath, folder));
+      // get only the controller file
+      const controllerFile = files.filter(f => /\.controller\.ts$/.test(f))[0];
+
+      if (!controllerFile) {
+        throw new Error(`Module ${folder} doesn't have a controller file`);
+      }
+
+      // import controller
+      const controller = require(`@/modules/${folder}/${controllerFile}`);
+
+      if (!controller.default) {
+        throw new Error(
+          'Controllers should be exported by: export default, no custom names',
+        );
+      }
+
+      controllers.push(controller.default);
+    });
+    return controllers;
+  } catch (error) {
+    throw error;
+  }
 }
 
 /** extract only route props */

e3f5dc06f12411ef2ebebf7d59aaaa2ea5fcf64f
Cristian Santiz <crisantizan@gmail.com>
2020-05-23 09:48:29 -0500

    feat: add types and schema

---
+++ src/common/joi-schemas/sanitize-db.schema.ts
@@ -0,0 +1,14 @@
+import joi from '@hapi/joi';
+import { SanitizeDB } from '@/modules/index/index.type';
+
+/** joi user create schema */
+export const sanitizeDBSchema = joi.object<SanitizeDB>({
+  columns: joi
+    .array()
+    .items(joi.string())
+    .required(),
+  filter: joi
+    .array()
+    .items(joi.string())
+    .required(),
+});
--- src/modules/index/index.type.ts
+++ src/modules/index/index.type.ts
@@ -2,3 +2,10 @@ export interface AnalizeFileColumns {
   filename: string;
   keyword: string;
 }
+
+export interface SanitizeDB {
+  /* column names */
+  columns: string[];
+  /* column names to filter */
+  filter: string[];
+}

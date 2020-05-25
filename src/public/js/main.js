559b6feb1f6f3620a2c64af0e4df365605fe8776
Cristian Santiz <crisantizan@gmail.com>
2020-05-25 18:47:20 -0500

    style: stepper styles

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,4 +1,12 @@
-const message = 'Hello world 3';
-document.addEventListener('DOMContentLoaded', function(event) {
-  console.log(message);
-});
+
+/** read file to upload */
+function readURL({ target }) {
+  if (!target.files.length) {
+    return;
+  }
+
+  const file = target.files[0];
+  const reader = new FileReader();
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {});

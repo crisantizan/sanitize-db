f2d8022aad3bfae350818a1c47b180f6e98d0de1
Cristian Santiz <crisantizan@gmail.com>
2020-05-27 12:26:34 -0500

    feat: save record in database

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -8,4 +8,16 @@ export class DbService {
 
     return rows;
   }
+
+  /** save records */
+  public async save(users: any[]) {
+    const columns = Object.keys(users[0]).join(', ');
+    const values = users.map(u => Object.values(u));
+
+    const [rows] = await conn.query(`INSERT INTO users (${columns}) VALUES ?`, [
+      values,
+    ]);
+
+    return rows;
+  }
 }
